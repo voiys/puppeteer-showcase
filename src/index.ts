612eed99a7ba028f3841dc108a693fe57ab1ffcf
Matej Ljubić <matej.ljubic@ngit.hr>
2020-01-29 09:23:16 +0100

    (entry) dodane jednostavne funkcije

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer';
+import { puppeteerCallback } from './types';
 
 const main: (puppeteerCallback: (...args: any[]) => any, closeAfterCallback?: boolean) => Promise<any>
 = async (puppeteerCallback, closeAfterCallback = true) => {
@@ -10,3 +11,37 @@ const main: (puppeteerCallback: (...args: any[]) => any, closeAfterCallback?: bo
     if (closeAfterCallback) await browser.close();
 };
 
+const pageScreenshotExample: puppeteerCallback
+= async (page) => {
+    await page.goto('https://example.com')
+
+    await page.screenshot({path: './data/example.png'});
+};
+
+const elementScrenshotExample: puppeteerCallback
+= async (page) => {
+    await page.goto('https://flashscore.com/tennis');
+
+    const selector = '.sportName.tennis'
+
+    await page.waitForSelector(selector);
+
+    const element = await page.$('.event__match')
+
+    await element!.screenshot({path: './data/tennisMatch.png'})
+}
+
+// const fullPageScreenshotExample: puppeteerCallback
+// = async (page) => {
+//     await page.goto()
+}
+
+// const PDFExample
+
+// const scrapePostsExample
+
+// const paginationExample
+
+// const SPAExample
+
+main(fullPageScreenshotExample);
